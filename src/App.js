cc74b4b241a1685645b7a989a5d2fbf905a4add3
Nishta Krishnan <nishtakrishnan@Nishtas-MacBook-Pro.local>
2024-02-29 14:14:55 -0600

    Worked on design for main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
+import React from 'react';
 import "./App.css";
 
 function App() {
-  return <div className="App">placeholder :)</div>;
+  return (
+    <div className="App">
+      <MainPageContent />
+    </div>
+  );
+}
+
+class MainPageContent extends React.Component {
+  render() {
+    return (
+      <div className="Main-page">
+        <h1>Placeranker</h1>
+        <div className="Search-bar">
+          <input type="text" placeholder="Type your city here" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
