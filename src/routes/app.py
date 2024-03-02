6d682028886772977e6fde9886a9e7107d34f88e
Akshay Ghosh <408.aagh0sh@gmail.com>
2024-03-01 19:40:00 -0600

    Added basic location query

---
+++ src/routes/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import requests
+
+# API Key for HERE API
+api_key = "WIyTOweLaqKkQrUl4HmxokjOSf-W2zhXmfUm2Sxd7zc"
+
+def get_location_data (query):
+    url = f"https://geocode.search.hereapi.com/v1/geocode?q={query}&apiKey={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+# # TO DO
+# # Location ID is the location ranking in our SQL database
+# def update_rank (location_id):
+
+
+if __name__ == "__main__":
+    get_location_data("Mia Za's Champaign")
