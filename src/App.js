893494cecb9235b864679865e383e3bc22fd77c1
Nishta <nishtak2@illinois.edu>
2024-04-09 10:55:32 -0500

    Combined JsonUploaderPage design with other pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
-import "./styles/MainPage.css";
+import "./styles/JsonUploaderPage.css";
 import Login from "./components/Login";
-import MainPage from "./components/MainPage";
+import JsonUploaderPage from "./components/JsonUploaderPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false); // Set to true if the user is logged in, false otherwise
+  const [loggedIn, setLoggedIn] = useState(true); // Set to true if the user is logged in, false otherwise
   return (
     <BrowserRouter>
       <Routes>
         {loggedIn ? (
-          <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<JsonUploaderPage />} />
         ) : (
           <>
             <Route
