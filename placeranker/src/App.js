aa6cbfe60a770a2a9eb449dc42d9eb6ff076d258
Nishta <nishtak2@illinois.edu>
2024-05-01 22:29:40 -0500

    updated display

--- placeranker/src/App.js
+++ placeranker/src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 import "./styles/JsonUploaderPage.css";
 import Login from "./components/Login";
 import MainPage from "./components/MainPage";
-import JsonUploaderPage from "./components/JsonUploaderPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Register from "./components/Register";
+import MapComponent from "./components/MapComponent";
+import coordinates from "./components/Coordinates"; // Import the coordinates array
+
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false); // Set to true if the user is logged in, false otherwise
   return (
-    <main className="font-inter">
+    <main className="font-inter min-w-[100vw] min-h-screen bg-eggshell flex items-center justify-center">
       <BrowserRouter>
         <Routes>
           {loggedIn ? (
@@ -23,11 +25,12 @@ function App() {
                 element={<Login setLoggedIn={setLoggedIn} />}
               />
               <Route path="*" element={<Login setLoggedIn={setLoggedIn} />} />
+              <Route path="/register" element={<Register />} />
             </>
           )}
         </Routes>
       </BrowserRouter>
-    </main>   
+    </main>
   );
 }
 export default App;
--- placeranker/src/components/MainPage.js
+++ placeranker/src/components/MainPage.js
@@ -1,10 +1,33 @@
 import React from "react";
 import "../styles/MainPage.css";
 import Header from "./Header";
-import JsonUploader from "./JsonUploader";
+import { useState } from "react";
 import UploadButton from "./UploadButton";
 
 const MainPage = ({ uploadFile }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file.type === "application/json") {
+      setSelectedFile(file);
+    } else {
+      alert("Please upload a JSON file.");
+    }
+  };
+
+  async function handleUpload(file) {
+    if (selectedFile) {
+      // Here you can handle the file upload, e.g., send it to a server
+      const formData = new FormData();
+      formData.append("files", file);
+      const requestOptions = { method: "POST", body: formData };
+      const response = await fetch("/api/upload", requestOptions);
+    } else {
+      console.error("No file selected");
+    }
+  }
+
   return (
     <div className="bg-eggshell font-inter text-[#29261B] flex flex-col min-h-screen items-center justify-between py-24">
       <div className="md:max-w-[600px] flex flex-col gap-10">
@@ -16,7 +39,7 @@ const MainPage = ({ uploadFile }) => {
             places by any metric in Google Maps. Placeranker lets you do that,
             and much more.
           </p>
-          <UploadButton></UploadButton>
+          <UploadButton handleUpload={handleUpload}></UploadButton>
         </div>
       </div>
     </div>
